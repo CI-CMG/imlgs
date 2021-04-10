a27563830d415ed139d3eb3205402db3a82b28fd
jcc <john.c.cartwright@comcast.net>
2021-04-10 16:37:19 -0600

    add ArcGIS

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,36 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React, { useEffect, useRef, useState } from 'react'
+import ArcGISMap from "@arcgis/core/Map"
+import MapView from "@arcgis/core/views/MapView"
+import esriConfig from '@arcgis/core/config.js';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // required if not in ./public/assets
+  // esriConfig.assetsPath = './esri_assets'; 
+  const mapDiv = useRef(null)
+
+  useEffect(() => {
+    if (mapDiv.current) {
+
+      const map = new ArcGISMap({
+        basemap: "oceans",
+      });
+
+      const view = new MapView({
+        map,
+        container: mapDiv.current,
+        zoom: 5,
+        center: [-90, 27]
+      });
+    } else {
+      console.warn('no mapDiv')
+    }
+  }, [])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="mapDiv" ref={mapDiv}>
+      </div>
     </div>
   )
 }
