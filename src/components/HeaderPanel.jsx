a1f9d0a400fa2650f0fd1098f02d883fe391d8a0
jcc <john.c.cartwright@comcast.net>
2021-06-14 15:33:48 -0600

    reference logo images from external URL to work around problem w/ assets directory in build

--- src/components/HeaderPanel.jsx
+++ src/components/HeaderPanel.jsx
@@ -10,13 +10,13 @@ function HeaderPanel() {
     return (
         <header className={baseClass}>
            <a href="https://www.ncei.noaa.gov/" title="National Centers for Environmental Information, National Oceanic and Atmospheric Administration">
-              <img className={`${baseClass}--nceiLogo`} src="/assets/map-banner.png" alt="National Centers for Environmental Information, National Oceanic and Atmospheric Administration"/>
+              <img className={`${baseClass}--nceiLogo`} src="https://maps.ngdc.noaa.gov/images/imlgs/map-banner.png" alt="National Centers for Environmental Information, National Oceanic and Atmospheric Administration"/>
             </a>
             <div className={`${baseClass}--titleDiv`}>
                 <span className={`${baseClass}--title`}>Index to Marine and Lacustrine Geological Samples (IMLGS)</span>
             </div>
             <span href="https://www.nsf.gov/div/index.jsp?div=OCE" title="external link to the National Science Foundation Ocean Sciences Division" target="_top">
-                <img className={`${baseClass}--nsfLogo`} src="/assets/NSFLogo.png" alt="US National Science Foundation Division of Ocean Sciences"/>
+                <img className={`${baseClass}--nsfLogo`} src="https://maps.ngdc.noaa.gov/images/imlgs/NSFLogo.png" alt="US National Science Foundation Division of Ocean Sciences"/>
             </span>
 
             {/* <Nav style={{margin:"5px", marginLeft:"75px"}} variant="pills" defaultActiveKey="/home">
