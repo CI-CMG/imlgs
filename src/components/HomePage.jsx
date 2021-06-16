1162f281edc98ad2ee5394b45b34959c5492318e
jcc <john.c.cartwright@comcast.net>
2021-06-16 08:48:18 -0600

    remove unneeded imports

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,26 +1,15 @@
 import React from "react"
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useRouteMatch,
-    useParams,
-    useLocation
-  } from "react-router-dom"
-import Button from 'react-bootstrap/Button'
 import FooterPanel from "./FooterPanel"
 import HeaderPanel from "./HeaderPanel"
-// import 'bootstrap/dist/css/bootstrap.min.css'
 import "./HomePage.css"
 import HomePanel from "./HomePanel"
 
 function HomePage() {
   console.log('inside Home...')
-
-    let match = useRouteMatch()
-    let location = useLocation();
-    console.log(location)
+  console.log(import.meta.env.TESTME)
+  // let match = useRouteMatch()
+  // let location = useLocation();
+    // console.log(location)
     return(
         <div className="HomePage">
           <HeaderPanel></HeaderPanel>
