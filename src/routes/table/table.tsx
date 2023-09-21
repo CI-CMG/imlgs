811abc0f38ccb562634a4619af5025ea2b948264
John Cartwright <jcc@leopard.local>
2023-09-21 09:50:35 -0600

    WIP: add query for table page

--- src/routes/table/table.tsx
+++ src/routes/table/table.tsx
@@ -1,8 +1,17 @@
 import './table.css'
-
+import { searchParamsToFilters } from '../../utilities'
+import { 
+  useLoaderData,
+ } from "react-router-dom"
+import { useQueryClient, useQuery, useQueries, UseQueryResult } from "@tanstack/react-query"
 
 export default function SamplesTable() {
-
+  const queryClient = useQueryClient()
+  console.log(queryClient.getDefaultOptions())
+  const url = new URL(window.location.href)
+  const filters = searchParamsToFilters(url.searchParams)
+  console.log('all filters: ', filters.toString())
+  
   return(
     <h2>Samples Table</h2>
   )
